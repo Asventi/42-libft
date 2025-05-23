78f0919914b1330aaf6f53b29192756c766413da
Pierre Jarnac <pjarnac@student.42lyon.fr>
2025-05-23 15:38:49 +0200

    feat: ft_split now accept a charset

--- includes/libft.h
+++ includes/libft.h
@@ -58,7 +58,7 @@ bool	ft_isstrcharset(const char *str, const char *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, const char *set);
 void	free_split(char **tab);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
--- src/str/ft_split.c
+++ src/str/ft_split.c
@@ -40,7 +40,7 @@ static char	**free_all(char **res, int c)
 	return (NULL);
 }
 
-static int	calc_alloc(char const *s, char c)
+static int	calc_alloc(char const *s, const char *set)
 {
 	int	in_word;
 	int	count;
@@ -49,31 +49,31 @@ static int	calc_alloc(char const *s, char c)
 	count = 0;
 	while (*s)
 	{
-		if (!in_word && *s != c)
+		if (!in_word && !ft_ischarset(*s, set))
 		{
 			count++;
 			in_word = 1;
 		}
-		else if (in_word && *s == c)
+		else if (in_word && !ft_ischarset(*s, set))
 			in_word = 0;
 		s++;
 	}
 	return (count);
 }
 
-static char	*split_strdup(char const *s, char c)
+static char	*split_strdup(char const *s, const char *set)
 {
 	char	*dup;
 	int		i;
 
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (s[i] && !ft_ischarset(s[i], set))
 		i++;
 	dup = ft_calloc(i + 1, sizeof (char));
 	if (!dup)
 		return (NULL);
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (s[i] && !ft_ischarset(s[i], set))
 	{
 		dup[i] = s[i];
 		i++;
@@ -81,28 +81,28 @@ static char	*split_strdup(char const *s, char c)
 	return (dup);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char const *s, const char *set)
 {
 	int		in_word;
 	int		count;
 	char	**res;
 
-	res = ft_calloc(calc_alloc(s, c) + 1, sizeof (char *));
+	res = ft_calloc(calc_alloc(s, set) + 1, sizeof (char *));
 	if (!res)
 		return (NULL);
 	in_word = 0;
 	count = 0;
 	while (*s)
 	{
-		if (!in_word && *s != c)
+		if (!in_word && !ft_ischarset(*s, set))
 		{
 			in_word = 1;
-			res[count] = split_strdup(s, c);
+			res[count] = split_strdup(s, set);
 			if (!res[count])
 				return (free_all(res, count));
 			count++;
 		}
-		else if (in_word && *s == c)
+		else if (in_word && ft_ischarset(*s, set))
 			in_word = 0;
 		s++;
 	}
